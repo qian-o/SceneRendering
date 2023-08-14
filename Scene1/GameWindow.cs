6a9f25a24dd9eb2e943549a944c6133ba56560b1
王 熙 <1324771795@qq.com>
2023-08-14 22:59:42 +0800

    wx

---
+++ Scene1/GameWindow.cs
@@ -0,0 +1,21 @@
+﻿using Core.Contracts.Windows;
+
+namespace Scene1;
+
+public class GameWindow : Game
+{
+    protected override void Load()
+    {
+
+    }
+
+    protected override void Update(double obj)
+    {
+
+    }
+
+    protected override void Render(double obj)
+    {
+
+    }
+}
