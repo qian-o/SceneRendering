07c55054efb62fd2fd53b066e007ef177c61cc33
王 熙 <1324771795@qq.com>
2023-08-15 16:09:23 +0800

    添加资源

---
+++ Core/Elements/SkyBox.cs
@@ -0,0 +1,31 @@
+﻿using Core.Contracts.Elements;
+using Core.Models;
+using Silk.NET.Maths;
+using Silk.NET.OpenGLES;
+using Program = Core.Tools.Program;
+
+namespace Core.Elements;
+
+public class SkyBox : BaseElement
+{
+    public override Mesh[] Meshes { get; }
+
+    public SkyBox(GL gl) : base(gl)
+    {
+        Vertex[] vertices = new Vertex[]
+        {
+            new Vertex(new Vector3D<float>(-0.5f, -0.5f, -0.5f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f, 0.0f)),
+            new Vertex(new Vector3D<float>(0.5f, -0.5f, -0.5f), new Vector3D<float>(0.0f,  0.0f, -1.0f), new Vector2D<float>(1.0f, 0.0f)),
+            new Vertex(new Vector3D<float>(0.5f,  0.5f, -0.5f), new Vector3D<float>(0.0f,  0.0f, -1.0f), new Vector2D<float>(1.0f, 1.0f)),
+            new Vertex(new Vector3D<float>(0.5f,  0.5f, -0.5f), new Vector3D<float>(0.0f,  0.0f, -1.0f), new Vector2D<float>(1.0f, 1.0f)),
+            new Vertex(new Vector3D<float>(-0.5f,  0.5f, -0.5f), new Vector3D<float>(0.0f,  0.0f, -1.0f), new Vector2D<float>(0.0f, 1.0f)),
+            new Vertex(new Vector3D<float>(-0.5f, -0.5f, -0.5f), new Vector3D<float>(0.0f,  0.0f, -1.0f), new Vector2D<float>(0.0f, 0.0f)),
+        };
+        uint[] indices = vertices.Select((a, b) => (uint)b).ToArray();
+    }
+
+    public override void Draw(Program program)
+    {
+        throw new NotImplementedException();
+    }
+}
