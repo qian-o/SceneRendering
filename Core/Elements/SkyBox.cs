58f7f458bd70125b3c6bd7965fcb9bf8000212e2
王 熙 <1324771795@qq.com>
2023-08-15 16:18:28 +0800

    更新顶点数据

--- Core/Elements/SkyBox.cs
+++ Core/Elements/SkyBox.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Contracts.Elements;
 using Core.Models;
+using Core.Tools;
 using Silk.NET.Maths;
 using Silk.NET.OpenGLES;
 using Program = Core.Tools.Program;
@@ -14,18 +15,65 @@ public class SkyBox : BaseElement
     {
         Vertex[] vertices = new Vertex[]
         {
-            new Vertex(new Vector3D<float>(-0.5f, -0.5f, -0.5f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f, 0.0f)),
-            new Vertex(new Vector3D<float>(0.5f, -0.5f, -0.5f), new Vector3D<float>(0.0f,  0.0f, -1.0f), new Vector2D<float>(1.0f, 0.0f)),
-            new Vertex(new Vector3D<float>(0.5f,  0.5f, -0.5f), new Vector3D<float>(0.0f,  0.0f, -1.0f), new Vector2D<float>(1.0f, 1.0f)),
-            new Vertex(new Vector3D<float>(0.5f,  0.5f, -0.5f), new Vector3D<float>(0.0f,  0.0f, -1.0f), new Vector2D<float>(1.0f, 1.0f)),
-            new Vertex(new Vector3D<float>(-0.5f,  0.5f, -0.5f), new Vector3D<float>(0.0f,  0.0f, -1.0f), new Vector2D<float>(0.0f, 1.0f)),
-            new Vertex(new Vector3D<float>(-0.5f, -0.5f, -0.5f), new Vector3D<float>(0.0f,  0.0f, -1.0f), new Vector2D<float>(0.0f, 0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f,  1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f, -1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>( 1.0f, -1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>( 1.0f, -1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>( 1.0f,  1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f,  1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            
+            new Vertex(new Vector3D<float>(-1.0f, -1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f, -1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f,  1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f,  1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f,  1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f, -1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+
+            new Vertex(new Vector3D<float>(1.0f, -1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(1.0f, -1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(1.0f,  1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(1.0f,  1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(1.0f,  1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(1.0f, -1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+
+            new Vertex(new Vector3D<float>(-1.0f, -1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f,  1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>( 1.0f,  1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>( 1.0f,  1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>( 1.0f, -1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f, -1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+
+            new Vertex(new Vector3D<float>(-1.0f,  1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>( 1.0f,  1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>( 1.0f,  1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>( 1.0f,  1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f,  1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f,  1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+
+            new Vertex(new Vector3D<float>(-1.0f, -1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f, -1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>( 1.0f, -1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>( 1.0f, -1.0f, -1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>(-1.0f, -1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f)),
+            new Vertex(new Vector3D<float>( 1.0f, -1.0f,  1.0f), new Vector3D<float>(0.0f), new Vector2D<float>(0.0f))
         };
         uint[] indices = vertices.Select((a, b) => (uint)b).ToArray();
+
+        Meshes = new Mesh[] { new Mesh(gl, vertices, indices, new Texture3D(gl, GLEnum.Rgba8, GLEnum.Repeat), new Texture3D(gl, GLEnum.Rgba8, GLEnum.Repeat)) };
+    }
+
+    public Texture3D GetDiffuseTex()
+    {
+        return Meshes[0].Diffuse3D!;
+    }
+
+    public Texture3D GetSpecularTex()
+    {
+        return Meshes[0].Specular3D!;
     }
 
     public override void Draw(Program program)
     {
-        throw new NotImplementedException();
+
     }
 }
