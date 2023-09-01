c40a3c1d45438db212913ba20dd995ccfdfef726
王 熙 <1324771795@qq.com>
2023-09-01 16:05:48 +0800

    vmd文件解析

--- Core/Elements/MikuMikuModel.cs
+++ Core/Elements/MikuMikuModel.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Contracts.Elements;
 using Core.Models;
-using Core.Models.MikuMikuDance;
+using Core.Models.MikuMikuDance.PMX;
+using Core.Models.MikuMikuDance.VMD;
 using Silk.NET.OpenGLES;
 using Program = Core.Tools.Program;
 
@@ -9,12 +10,18 @@ namespace Core.Elements;
 public unsafe class MikuMikuModel : BaseElement
 {
     private readonly PMXFile _pmx;
+    private readonly VMDFile? _vmd;
 
     public override Mesh[] Meshes { get; }
 
     public MikuMikuModel(GL gl, string pmxPath, string? vmdPath = null) : base(gl)
     {
         _pmx = new PMXFile(pmxPath);
+
+        if (vmdPath != null)
+        {
+            _vmd = new VMDFile(vmdPath);
+        }
 
         Meshes = Array.Empty<Mesh>();
     }
