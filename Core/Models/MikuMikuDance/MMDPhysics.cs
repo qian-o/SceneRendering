d172c1566cef1b8cbb2aed46741cf6f5268ce2e6
王 熙 <1324771795@qq.com>
2023-09-05 18:29:43 +0800

    定义MMDPhysics应该使用的变量

--- Core/Models/MikuMikuDance/MMDPhysics.cs
+++ Core/Models/MikuMikuDance/MMDPhysics.cs
@@ -1,5 +1,18 @@
-﻿namespace Core.Models.MikuMikuDance;
+﻿using BulletSharp;
+
+namespace Core.Models.MikuMikuDance;
 
 public class MMDPhysics
 {
+    private BroadphaseInterface broadphase;
+    private DefaultCollisionConfiguration collisionConfiguration;
+    private CollisionDispatcher dispatcher;
+    private SequentialImpulseConstraintSolver solver;
+    private DiscreteDynamicsWorld world;
+    private CollisionShape collisionShape;
+    private MotionState groundMS;
+    private RigidBody groundRB;
+    private OverlapFilterCallback filterCB;
+    private double fps;
+    private int maxSubStepCount;
 }
